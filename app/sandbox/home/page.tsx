ae272f6b8c07d5b1fe869544a4619f7934547211
mylawnhq <team@mylawnhq.com>
2026-02-07 10:08:26 -0500

    Add 'Free' to locked tools CTA to reduce friction

--- app/sandbox/home/page.tsx
+++ app/sandbox/home/page.tsx
@@ -606,7 +606,7 @@ function HomePageContent() {
                 <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
                 </svg>
-                Sign Up to Unlock
+                Sign Up Free to Unlock
               </Link>
             </div>
 
@@ -1050,7 +1050,7 @@ function HomePageContent() {
                   <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
                   </svg>
-                  Sign Up to Unlock
+                  Sign Up Free to Unlock
                 </Link>
               </div>
 
